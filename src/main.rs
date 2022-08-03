35f319eea5ccf83b0505a15b342a02284a539ec2
Liam-Wirth <liam.wirth1@outlook.com>
2022-08-03 00:33:01 -0700

    added the database file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-const DB_PATH: str = ""; //TODO set up a database file for later
+const DB_PATH: str = "/database.json"; //TODO set up a database file for later
 
 struct Item {
     //TODO maybe implement stntrading shit here?
@@ -13,6 +13,12 @@ struct Item {
     scrap_tf_price_ref: u32,
     scrap_tf_price_key: f32,
     scrap_tf_price_pretty: (u16, u32),
+}
+pub enum errors {
+    #[error("error reading the DB file: {0}")]
+    ReadDBError(#[from] io::Error),
+    #[error("error parsing the DB file: {0}")]
+    ParseDBError(#[from] serde_json::Error),
 }
 
 fn main() {}
